de09ffe6a09206cc447dac9b7acdcaf89fe84e37
SenneRobbrecht <senne.robbrecht@dodentocht.be>
2025-12-20 13:45:47 +0100

    feat: working on login

--- src/admin/router/index.js
+++ src/admin/router/index.js
@@ -1,8 +1,15 @@
 import { createRouter, createWebHistory } from "vue-router";
 import AdminLayout from "../layout/AdminLayout.vue";
 import AdminDashboard from "../pages/AdminDashboard.vue";
+import Login from "../pages/LoginPage.vue";
+
 
 const routes = [
+      {
+    path: "/login",
+    name: "Login",
+    component: Login
+  },
   {
     path: "/admin",
     component: AdminLayout,
