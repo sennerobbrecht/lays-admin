21f2d0de6ce4a95855b56418ee027ea0dc625706
SenneRobbrecht <senne.robbrecht@dodentocht.be>
2025-12-20 13:35:58 +0100

    feat: admin dashboard

---
+++ src/admin/pages/AdminDashboard.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Admin Dashboard</h1>
+    <p>Welkom! Dit is het admin overzicht.</p>
+  </div>
+</template>
+
+<style>
+h1 {
+  font-size: 28px;
+  margin-bottom: 10px;
+}
+</style>
